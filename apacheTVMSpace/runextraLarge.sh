65579ba5268b47dff5ee1f9047725f590e2a0851
Praveen Paramasivam <pparamasivam@gpulogin2.lcrc.anl.gov>
2023-07-18 16:01:12 -0500

    modified run script

---
+++ apacheTVMSpace/runextraLarge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=10:00:00
+#SBATCH --job-name=ytopt
+#SBATCH --output=slurmLog/out/gpu-matmul-tvm.%j.out
+#SBATCH --error=slurmLog/err/gpu-matmul-tvm.%j.err
+#SBATCH --gres=gpu:8
+#SBATCH --account=EE-ECP
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=pparamasivam@anl.gov
+
+source /home/pparamasivam/anaconda3/bin/activate
+conda activate tvm
+
+python /home/pparamasivam/ytune/ytopt-libensemble/apacheTVMSpace/matMul_Baseline.py --size=XL
+python /home/pparamasivam/ytune/ytopt-libensemble/apacheTVMSpace/matMul_GATuner.py --size=XL
+python /home/pparamasivam/ytune/ytopt-libensemble/apacheTVMSpace/matMul_GridSearch.py --size=XL
+python /home/pparamasivam/ytune/ytopt-libensemble/apacheTVMSpace/matMul_RandTuner.py --size=XL
