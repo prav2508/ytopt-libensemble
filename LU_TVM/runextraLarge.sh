c373568c8961b7fe6ff9f9d2b8ce4bd3b2272ec1
Praveen Paramasivam <pprav2508@gmail.com>
2023-08-02 23:45:14 -0500

    run updates

--- LU_TVM/runextraLarge.sh
+++ LU_TVM/runextraLarge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=10:00:00
+#SBATCH --time=06:00:00
 #SBATCH --job-name=ytopt
 #SBATCH --output=slurmLog/out/gpu-matmul-tvm.%j.out
 #SBATCH --error=slurmLog/err/gpu-matmul-tvm.%j.err
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 #SBATCH --account=EE-ECP
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=pparamasivam@anl.gov
